daf18aea928a11f5774f4cd917a157cd3f1233b1
PAULINA GRABSKA <paulinagrabskainf@gmail.com>
2023-10-22 22:21:51 +0200

    Pracadomowa2

---
+++ 03-ControlStructures/zad17.py
@@ -0,0 +1,10 @@
+sum_of_numbers = 0
+current_number = 1
+
+# While loop to calculate the sum
+while current_number <= 5:
+    sum_of_numbers += current_number
+    current_number += 1
+
+# Print the result
+print("The sum of numbers in the range <1, 5> is:", sum_of_numbers)
