1b11ee142ae6bd3b1bec6bc4a9c160467bb43b04
PAULINA GRABSKA <paulinagrabskainf@gmail.com>
2023-10-24 22:47:06 +0200

    Praca Domowa1

--- 02-TypesAndVariables/zad3cz.py
+++ 02-TypesAndVariables/zad3cz.py
@@ -2,6 +2,6 @@ father=5000
 mother=7000
 numofpeople=3
 totalincome=father+mother
-int(incperperson=totalincome/numofpeople)
+incperperson=totalincome/numofpeople
 s=f"Enter father's income: {father}\n Enter mother's income: {mother} \n Enter number of people: {numofpeople}\n Total income: {totalincome}\n Income per person: {incperperson}\n "
 print(s)
